779dcb061d9a59172a2503a3bc009e8cffdeadf8
kittors <yuan364299311@gmail.com>
2026-05-04 08:37:22 +0800

    chore: update management panel assets

--- assets/channel-groups-DgYUkP20.js
+++
@@ -1,1 +0,0 @@
-import{e as t}from"./index-4T_n3oqv.js?v=issue77-management-context";const n={async list(){const a=(await t.get("/channel-groups"))?.items;return Array.isArray(a)?a:[]}};export{n as c};
